ef60fa01db275ca36f6044fa66ffa663a7d6f381
Adrià Vallès <adria@letgo.com>
2017-02-10 16:35:16 +0100

    Result

--- src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
+++ src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
@@ -4,8 +4,25 @@ import com.letgo.scala_course.domain.Message
 
 class MessageCensor(forbiddenKeywords: Set[String]) {
 
-  private val filterRule: Message => Message = ???
+  private val filterRule: Message => Message = { message =>
+    forbiddenKeywords.foldLeft(message) { (textMessage, forbiddenKeyword) =>
+      message.copy(text = textMessage.text.replace(forbiddenKeyword, "").replace("  ", " ").trim)
+    }
+  }
+
+  //  private val filterRule: Message => Message = { message =>
+  //    forbiddenKeywords.foldLeft(message) { (textMessage, forbiddenKeyword) =>
+  //      Message(textMessage.text.replace(forbiddenKeyword, "").replace("  ", " ").trim)
+  //    }
+  //  }
+
+  //  private val filterRule: Message => Message = { message =>
+  //    var text = message.text
+  //    forbiddenKeywords.foreach(forbiddenKeyword => text = text.replace(forbiddenKeyword, "")).replace("  ", " ").trim
+  //    Message(text)
+  //  }
 
   def filterMessages(messages: Seq[Message]): Seq[Message] = messages.map(filterRule)
+
 }
 
